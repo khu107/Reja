13fc247149749e07f4b786f399d50298892a9d41
metablog <khu10735@naver.com>
2024-02-03 00:53:24 +0900

    feat: calculate

---
+++ hisob.js
@@ -0,0 +1,21 @@
+// File module
+
+let hisob = {};
+
+hisob.kupaytirish = (a, b) => {
+  return a * b;
+};
+
+hisob.bulish = (a, b) => {
+  return a / b;
+};
+
+hisob.qushish = (a, b) => {
+  return a - b;
+};
+
+hisob.ayirish = (a, b) => {
+  return a * b;
+};
+
+module.exports = hisob;
---
+++ index.js
@@ -0,0 +1,8 @@
+const calculate = require("./hisob.js");
+
+const natija = calculate.kupaytirish(80, 20);
+console.log("natija", natija);
+
+console.log("----------------");
+const natija2 = calculate.qushish(70, 20);
+console.log("natija2", natija2);

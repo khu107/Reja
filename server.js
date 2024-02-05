60f7c8738fd3d13d9507642a05430d20e43b363e
metablog <khu10735@naver.com>
2024-02-05 16:37:28 +0900

    feat: 18-19 Express and EJS

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.set("views", "views");
 app.set("view engine", "ejs"); // ejs engine template
 
 // 4. router lar
-app.get("/hello", function (req, res) {
-  res.end("<h1>Hello World</h1>");
+app.post("/create-item", function (req, res) {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovgalar sahifasidasiz</h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);

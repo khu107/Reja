f530c543eec3576d3d3b8a078dd51b1204a914a7
metablog <khu10735@naver.com>
2024-02-08 00:10:32 +0900

    feat: author page

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("err:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 // 1. express ga kirib kelayotgan malumotlarga bogliq bulgan kodlar.
 app.use(express.static("public")); // harqanday  browserdan kirib kelgan request uchun public fulderi ochiq degan manoni anglatadi.
 app.use(express.json()); // kirib kelayotgan json formatdagi datani object formtga uzgartirib beradi.
@@ -15,12 +25,12 @@ app.set("view engine", "ejs"); // ejs engine template
 
 // 4. router lar
 app.post("/create-item", function (req, res) {
-  console.log(req.body);
-  res.json({ test: "success" });
+  // console.log(req.body);
+  // res.json({ test: "success" });
 });
 
-app.get("/", function (req, res) {
-  res.render("harid");
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 const server = http.createServer(app);

9d4cd3bb0616e9e57dfe6133538ccbe477c92f4a
metablog <khu10735@naver.com>
2024-02-17 00:48:13 +0900

    feat: Task - D

--- train.js
+++ train.js
@@ -1,42 +1,7 @@
-// class Shop {
-//   constructor(non, cola, lagmon) {
-//     this.non = non;
-//     this.cola = cola;
-//     this.lagmon = lagmon;
-//   }
+// Task - D
+function solution(str1, str2) {
+  const sortString = (str) => str.split("").sort().join("");
+  return sortString(str1) === sortString(str2);
+}
 
-//   qabul(type, num) {
-//     if (type === "non") {
-//       this.non = this.non + num;
-//     } else if (type === "cola") {
-//       this.cola = this.cola + num;
-//     } else {
-//       this.lagmon = this.lagmon + num;
-//     }
-//   }
-//   sotish(type, num) {
-//     if (type === "non") {
-//       this.non = this.non - num;
-//     } else if (type === "cola") {
-//       this.cola = this.cola - num;
-//     } else {
-//       this.lagmon = this.lagmon - num;
-//     }
-//   }
-//   qoldiq() {
-//     return `hozir ${new Date().getHours()} : ${new Date().getMinutes()} da ${
-//       this.non
-//     } ta non, ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!`;
-//   }
-// }
-
-// const shop = new Shop(4, 5, 2);
-// console.log(shop);
-// shop.qabul("cola", 2);
-// shop.qabul("lagmon", 5);
-// console.log(shop);
-// console.log(shop.qoldiq());
-// shop.sotish("cola", 1);
-// shop.sotish("lagmon", 1);
-
-// console.log(shop.qoldiq());
+console.log(solution("mitgroup", "gmtiprou"));
